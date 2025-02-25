b8634085d054fd71e2c38340df0e77284a8df04e
Immagorst <nmh31052005179@gmail.com>
2025-02-25 20:43:04 +0700

    okkkk

---
+++ frmDonViTinh.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmDonViTinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDVT = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDVT = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDVT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDVT
+            // 
+            this.dgvDVT.AllowUserToAddRows = false;
+            this.dgvDVT.AllowUserToDeleteRows = false;
+            this.dgvDVT.AllowUserToOrderColumns = true;
+            this.dgvDVT.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDVT.BackgroundColor = System.Drawing.Color.White;
+            this.dgvDVT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDVT.Location = new System.Drawing.Point(12, 141);
+            this.dgvDVT.MultiSelect = false;
+            this.dgvDVT.Name = "dgvDVT";
+            this.dgvDVT.ReadOnly = true;
+            this.dgvDVT.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDVT.Size = new System.Drawing.Size(776, 268);
+            this.dgvDVT.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(532, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Đơn vị tính";
+            // 
+            // txtDVT
+            // 
+            this.txtDVT.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDVT.Location = new System.Drawing.Point(598, 84);
+            this.txtDVT.Name = "txtDVT";
+            this.txtDVT.Size = new System.Drawing.Size(100, 20);
+            this.txtDVT.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnSave.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSave.Location = new System.Drawing.Point(719, 83);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(69, 20);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(595, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "QUẢN LÝ DANH SÁCH ĐƠN VỊ TÍNH";
+            // 
+            // frmDonViTinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtDVT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDVT);
+            this.Name = "frmDonViTinh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đơn vị tính";
+            this.Load += new System.EventHandler(this.frmDonViTinh_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDVT)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDVT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDVT;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ frmDonViTinh.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.TayVinSieuVipDataSetTableAdapters;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmDonViTinh : Form
+    {
+        private TayVinSieuVipDataSet dataset;
+        private DonViTinhTableAdapter dvtAdapter;
+        private string nhanvien = "admin"; // Mặc định admin, sau sẽ truyền từ đăng nhập
+
+        public frmDonViTinh()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDonViTinh_Load(object sender, EventArgs e)
+        {
+            dataset = new TayVinSieuVipDataSet(); // Khởi tạo dataset
+            dvtAdapter = new DonViTinhTableAdapter(); // Khởi tạo adapter
+            LoadData(); // Tải dữ liệu từ CSDL
+        }
+
+        private void LoadData()
+        {
+            dvtAdapter.Fill(dataset.DonViTinh); // Đổ dữ liệu từ TableAdapter vào DataTable
+            dgvDVT.DataSource = dataset.DonViTinh; // Hiển thị dữ liệu lên DataGridView
+            foreach (DataGridViewColumn col in dgvDVT.Columns)
+            {
+                col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter; // Căn giữa nội dung
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter; // Căn giữa tiêu đề
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtDVT.Text)) // Nếu TextBox không rỗng
+            {
+                // Thêm dòng mới vào DataTable
+                DataRow newRow = dataset.DonViTinh.NewRow();
+                newRow["TenDVT"] = txtDVT.Text; // Thêm tên đơn vị tính
+
+                dataset.DonViTinh.Rows.Add(newRow); // Thêm vào DataTable
+                dvtAdapter.Update(dataset.DonViTinh); // Cập nhật CSDL
+
+                MessageBox.Show("Đã thêm đơn vị tính thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                LoadData(); // Tải lại dữ liệu để cập nhật DataGridView
+                txtDVT.Text = ""; // Reset lại textbox sau khi thêm thành công
+                txtDVT.Focus(); // Đưa con trỏ về TextBox để nhập tiếp
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đơn vị tính", "Chú ý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
